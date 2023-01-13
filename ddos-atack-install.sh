75bb184dbc2ae34fcafeffdcd988bb922a0afa56
HED <96006135+HED69@users.noreply.github.com>
2023-01-13 21:30:52 +0100

    Update ddos-atack-install.sh

--- ddos-atack-install.sh
+++ ddos-atack-install.sh
@@ -15,7 +15,7 @@ sudo apt install figlet -y
 sudo apt install python-colorama -y
 sudo apt install python3-colorama -y
 clear
-figlet "installing"
+figlet "installing..."
 echo "This script is not owned by me, you can access the original from: https://github.com/biyivi/biyivi_ataque_DDos"
 git clone https://github.com/biyivi/biyivi_ataque_DDos
 cd biyivi_ataque_DDos
